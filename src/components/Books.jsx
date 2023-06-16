75fb9679a278cc383b591ece5bdb58e7efb03559
ibktommy <atomquartz@gmail.com>
2023-06-16 17:34:32 +0100

    use ternary-operator to check if the booklist-array is empty

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -3,31 +3,35 @@ import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 const Books = ({ bookListProps }) => {
 	return (
 		<div className="books">
-			{bookListProps.map((bookItem) => {
-				const { id, title, author, image } = bookItem;
-				return (
-					<article className="book-item" key={id}>
-						<input type="checkbox" name="" id="" />
+			{bookListProps.length === 0 ? (
+				<h3 className="empty-header">Your booklist is empty, add books</h3>
+			) : (
+				bookListProps.map((bookItem) => {
+					const { id, title, author, image } = bookItem;
+					return (
+						<article className="book-item" key={id}>
+							<input type="checkbox" name="" id="" />
 
-						<div className="book-details">
-							<img src={image} alt={title} />
-							<div className="book-details-text">
-								<h4>{title}</h4>
-								<p>{author}</p>
+							<div className="book-details">
+								<img src={image} alt={title} />
+								<div className="book-details-text">
+									<h4>{title}</h4>
+									<p>{author}</p>
+								</div>
 							</div>
-						</div>
 
-						<div className="book-cta">
-							<button className="edit btn">
-								<AiFillEdit />
-							</button>
-							<button className="delete btn">
-								<AiFillDelete />
-							</button>
-						</div>
-					</article>
-				);
-			})}
+							<div className="book-cta">
+								<button className="edit btn">
+									<AiFillEdit />
+								</button>
+								<button className="delete btn">
+									<AiFillDelete />
+								</button>
+							</div>
+						</article>
+					);
+				})
+			)}
 		</div>
 	);
 };
