917d92f787717205ab49c108a6d4a74b9c2d271c
ibktommy <atomquartz@gmail.com>
2023-06-16 17:53:34 +0100

    Added function that toggle the checked-state-value when the input-check is clicked

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,6 +1,14 @@
+import { useState } from "react";
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 
 const Books = ({ bookListProps, deleteBookProps }) => {
+
+  const [checked, setIsChecked] = useState(false)
+
+  function checkedHandler(e) {
+    setIsChecked(!checked)
+  }
+
 	return (
 		<div className="books">
 			{bookListProps.length === 0 ? (
@@ -10,13 +18,17 @@ const Books = ({ bookListProps, deleteBookProps }) => {
 					const { id, title, author, image } = bookItem;
 					return (
 						<article className="book-item" key={id}>
-							<input type="checkbox" name="" id="" />
+							<input type="checkbox" onChange={checkedHandler} />
 
 							<div className="book-details">
-								<img src={image} alt={title} />
+								<img
+									src={image}
+									alt={title}
+									className={checked ? "checked" : ""}
+								/>
 								<div className="book-details-text">
-									<h4>{title}</h4>
-									<p>{author}</p>
+									<h4 className={checked ? "checked" : ""}>{title}</h4>
+									<p className={checked ? "checked" : ""}>{author}</p>
 								</div>
 							</div>
 
